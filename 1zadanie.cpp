8283e0fb440d4083fb0c7ce809bf4a7bb9b055fb
Artemazarov1 <146062675+Artemazarov1@users.noreply.github.com>
2023-10-09 12:23:01 +0300

    Add files via upload

---
+++ 1zadanie.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+	setlocale(0, "");
+	int num;
+	cout << "Введите число:";
+	cin >> num;
+
+	int kvadrat = num * num;
+	int kyb = num * num * num;
+
+	cout << "Квадрат числа:" << kvadrat << endl;
+	cout << "Куб числа:" << kyb << endl;
+
+	return 0;
+}
